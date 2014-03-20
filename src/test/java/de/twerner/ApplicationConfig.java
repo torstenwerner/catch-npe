7721d616ddc0c8ed821d1ae5f09f861ce9c18186
Torsten Werner <torsten.werner@westernacher.com>
2014-03-20 18:13:47 +0100

    remove interface

--- src/test/java/de/twerner/ApplicationConfig.java
+++ src/test/java/de/twerner/ApplicationConfig.java
@@ -1,11 +1,9 @@
 package de.twerner;
 
-import org.springframework.aop.aspectj.annotation.AnnotationAwareAspectJAutoProxyCreator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
 
 @Configuration
 @ComponentScan("de.twerner")
@@ -13,6 +11,6 @@ import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
 public class ApplicationConfig {
     @Bean
     public Helper helper() {
-        return new HelperImpl();
+        return new Helper();
     }
 }
--- src/test/java/de/twerner/Helper.java
+++ src/test/java/de/twerner/Helper.java
@@ -1,9 +1,20 @@
 package de.twerner;
 
-/**
- * Created by torstenwerner on 20.03.14.
- */
-public interface Helper {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Helper {
     @CatchNullPointerException
-    String helper(Object arg);
+    public String helper(Object arg) {
+        return arg.toString();
+    }
+
+    public static void main(String args[]) {
+        final ApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfig.class);
+        final Helper helper = context.getBean(Helper.class);
+        final Logger logger = LoggerFactory.getLogger(Helper.class);
+        logger.info(helper.helper("Hello"));
+    }
 }
